4e461ed79cedea21db62d34816aa1385782730af
artjacob <aacj03@gmail.com>
2019-06-18 02:26:49 -0300

    Definições de cores unificadas para a criação do tema escuro

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,7 +21,8 @@ self.addEventListener("install", (event) => {
                 "/cabin-700.woff2",
                 "/material-icons.woff2",
                 "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js",
-                "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
+                "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js",
+                "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/pt-br.js"
             ]))
     );
 });
