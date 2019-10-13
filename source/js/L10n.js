881ef65c9ba749d4fc8d259e396ce5119616c184
artjacob <jacob@emitremmus.com>
2019-10-13 04:09:45 -0300

    Criação de arquivo para traduções da interface

---
+++ source/js/L10n.js
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// localization ////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const language = (navigator.language ? navigator.language.substr(0, 2) : "pt");
+const language = "pt";
+
+// if (language === "pt") {
+    moment.locale("pt-br");
+// } else {
+//     moment.locale(language);
+// }
+
+const L10n = {
+    "pt": {
+        "next-departures": "Próximos trens",
+        "real-time": "Tempo real",
+        "are-you-at": "Está em",
+        "direction": "Sentido",
+        "now": "Agora",
+        "train": "Trem",
+        "line": "Linha",
+        "updated": "Atualizado",
+        "out-of-operation": "Fora de operação",
+        "operation-hours": "O VLT Carioca circula<br> todos os dias das 6h à 0h",
+        "ago": "atrás",
+        "stations": "Estações",
+        "find-nearest-station": "Localizar estação mais próxima",
+        "auto-theme": "Tema automático",
+        "light-theme": "Tema claro",
+        "dark-theme": "Tema escuro",
+        "offline": "Sem conexão",
+        "status-ok": "Operação normal"
+    }
+    // "en": {
+    //     "next-departures": "Next trains",
+    //     "real-time": "Real Time",
+    //     "are-you-at": "Are you at",
+    //     "direction": "Direction",
+    //     "now": "Now",
+    //     "train": "Train",
+    //     "line": "Line",
+    //     "updated": "Updated",
+    //     "out-of-operation": "Service closed",
+    //     "operation-hours": "VLT Carioca runs<br> daily from 6am to 12am",
+    //     "ago": "ago",
+    //     "stations": "Stations",
+    //     "find-nearest-station": "Find your nearest station",
+    //     "auto-theme": "Auto theme",
+    //     "light-theme": "Light theme",
+    //     "dark-theme": "Dark theme",
+    //     "offline": "Offline",
+    //     "status-ok": "Good service"
+    // },
+    // "es": {
+
+    // }
+};
